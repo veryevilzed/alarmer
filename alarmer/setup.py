526b5047b3b8b8b3ead9a3eacd223c5ddec6926c
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2014-11-18 22:30:17 +0300

    Initial

---
+++ alarmer/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+from distutils.core import setup
+
+setup(name='Alarmer',
+      version='1.0',
+      description='Python Alarm Utilities',
+      author='Dmitry Vysochin',
+      author_email='dmitry.vysochin@gmail.com',
+      url='https://github.com/veryevilzed/alarmer',
+      packages=['alarmer', ],
+     )
