c7fe17ad9b4f13b9d68e3e20f0290271e285ced2
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2014-11-19 15:12:55 +0300

    SysInfo

--- alarmer/src/alarmer/checkers/ram.py
+++
@@ -1,7 +0,0 @@
-#coding:utf-8
-
-import psutils
-
-class Ram:
-    def __init__(self):
-        pass
---
+++ alarmer/src/alarmer/checkers/sysinfo.py
@@ -0,0 +1,46 @@
+#coding:utf-8
+
+import psutils, os, re
+from plumbum import local
+
+
+class Ram:
+    def __init__(self):
+        pass
+
+    def get(self):
+        self.value = psutil.virtual_memory().percent
+
+
+class CPU:
+    def __init__(self):
+        pass
+
+    def get(self):
+        self.value = psutil.cpu_percent()
+
+
+class Disk:
+    def __init__(self):
+        pass
+        
+    def get(self):
+        p = os.statvfs(self._options.get("target", "/") )
+        self.value = p.f_bfree / float(p.f_blocks)
+
+
+
+class Ping:
+    def __init__(self):
+        self.re = re.compile(r"(\d+\.\d+)% packet loss")
+        self.ping = local['ping']['-c1']
+
+    def get(self):
+        try:
+            text = self.ping(self._options.get("target", timeout=self._options.get("timeout", 2)))
+            m = self.re.search(text)
+            if not m:
+                self.value = 100.0
+            self.value = float(m.group(1))
+        except:
+            self.value = 100.0
